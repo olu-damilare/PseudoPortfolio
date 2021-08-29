7dde62269544253f5819ada39b68ecbca0bc6399
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-29 05:30:39 +0100

    changed rendered component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Articles from './articles';
+import About from './about';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <About />
+    {/* <Articles /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
