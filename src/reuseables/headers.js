5fa497a59a0f9f9109a71e5687c77341fcf55233
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-29 05:28:20 +0100

    changed anchor tags to link component

--- src/reuseables/headers.js
+++ src/reuseables/headers.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import Button from './button'
 import heart from '../img/heart.png';
+import {BrowserRouter as Router,
+    Switch,
+    Route, Link } from 'react-router-dom'
+import Articles from '../articles';
 
  
 const Header = () => {
     return (
-        <header>
-            <div className="header_left">
-                <img src={heart} alt = "heart"/>
-                <p className="name">Olu Jolayemi</p>
-            </div>
-            <div className="header_right">
-                <a href="#" target="_blank" id="article">Articles</a>
-                <a href="#" target="_blank" id="snippets">Snippets</a>
-                <a href="#" target="_blank" id="about">About</a>
-                <Button name="Contact" className="contact"/>
-            </div>
-        </header>
+        <Router>
+            <header>
+                <div className="header_left">
+                    <img src={heart} alt = "heart"/>
+                    <p className="name">Olu Jolayemi</p>
+                </div>
+                <div className="header_right">
+                    <Link className = "link" to="/articles">Articles</Link>
+                    <Link className = "link" to="/#/">Snippets</Link>
+                    <Link className = "link" to="/#/">About</Link>
+                    {/* <a href="../articles.js" target="_blank" id="article">Articles</a> */}
+                    {/* <a href="#" target="_blank" id="snippets">Snippets</a>
+                    <a href="#" target="_blank" id="about">About</a> */}
+                    <Button name="Contact" className="contact"/>
+                </div>
+
+                   <Switch> 
+                    <Route path="/articles" component = {Articles}/> 
+
+                    {/* <Route path="../articles" component={Articles} /> */}
+                    </Switch>
+            </header>
+        </Router>
     )
 }
 
