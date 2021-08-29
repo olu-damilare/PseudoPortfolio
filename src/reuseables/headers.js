2a36252dcac7195a63656622f88f9e7e888e60b5
Damilare Jolayemi <djolayemi@gmail.com>
2021-08-29 18:09:00 +0100

    modified content of headers component

--- src/reuseables/headers.js
+++ src/reuseables/headers.js
@@ -9,29 +9,20 @@ import Articles from '../articles';
  
 const Header = () => {
     return (
-        <Router>
-            <header>
-                <div className="header_left">
-                    <img src={heart} alt = "heart"/>
-                    <p className="name">Olu Jolayemi</p>
-                </div>
-                <div className="header_right">
-                    <Link className = "link" to="/articles">Articles</Link>
-                    <Link className = "link" to="/#/">Snippets</Link>
-                    <Link className = "link" to="/#/">About</Link>
-                    {/* <a href="../articles.js" target="_blank" id="article">Articles</a> */}
-                    {/* <a href="#" target="_blank" id="snippets">Snippets</a>
-                    <a href="#" target="_blank" id="about">About</a> */}
-                    <Button name="Contact" className="contact"/>
-                </div>
+        <header>
+            <div className="header_left">
+                <img src={heart} alt = "heart"/>
+                <p className="name"><Link to="/"> Olu Jolayemi </Link> </p>
+            </div>
+            <div className="header_right">
+                <Link className = "link" to="/articles">Articles</Link>
+                <Link className = "link" to="/#/">Snippets</Link>
+                <Link className = "link" to="/about">About</Link>
+                <Button name="Contact" className="contact"/>
+            </div>
 
-                   <Switch> 
-                    <Route path="/articles" component = {Articles}/> 
-
-                    {/* <Route path="../articles" component={Articles} /> */}
-                    </Switch>
-            </header>
-        </Router>
+              
+        </header>
     )
 }
 
